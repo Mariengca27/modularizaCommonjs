c09045ea34786b9818a50a1652fc6db4376b7fee
Mari <53942555+Mariengca27@users.noreply.github.com>
2020-12-22 08:30:30 -0300

    Add files via upload

---
+++ commonTeste.js
@@ -0,0 +1,7 @@
+// --22/12/2020-- arquivo criado para conter a lógica do módulo CommonJs
+
+
+var msg = "Este módulo contém apenas uma linha para testar o CommonJs.";
+
+//Instrução de modulo de exportação + o que ele quer exportar abaixo :
+module.exports = msg;
